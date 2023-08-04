7981242ad6d74c0bd827c79cde7d4b6d806fea2c
Vincent Schneider <vincent.schneider@sudile.com>
2023-08-04 08:10:41 -0700

    improved useability and added fingerprint submodule for later use

--- main.py
+++ main.py
@@ -1,41 +1,61 @@
 import json
 import os
+import argparse
 
 import requests
 
 with open('plugins/plugintype_map.json', 'r') as f:
-    plugintypes = json.load(f)
-
-url = "https://moodle.example.com"
+    plugin_types = json.load(f)
 
 
 def get_plugin_type_url(plugintype):
-    return plugintypes[plugintype]
+    return plugin_types[plugintype]
 
 
-def test_plugin(pluginname, plugintype):
-    data = requests.get(url + get_plugin_type_url(plugintype) + "/" + pluginname + "/version.php")
+def test_plugin(host, plugin_name, plugin_type, extended_check):
+    data = requests.get(host + get_plugin_type_url(plugin_type) + "/" + plugin_name + "/version.php")
     if data.status_code != 200:
         return False
-    # changes_map = ["CHANGES.md", "CHANGES.txt", "CHANGES.html", "CHANGES"]
-    # for changes in changes_map:
-    #    data = requests.get(url + get_plugin_type_url(plugintype) + "/" + pluginname + "/" + changes)
-    #    if data.status_code == 200:
-    #        print(data.content.decode())
+
+    if extended_check:
+        # changes_map = ["CHANGES.md", "CHANGES.txt", "CHANGES.html", "CHANGES"]
+        # for changes in changes_map:
+        #     data = requests.get(host + get_plugin_type_url(plugin_type) + "/" + plugin_name + "/" + changes)
+        #     if data.status_code == 200:
+        #         print(data.content.decode())
+        #         break
+        # git checken
+        # index of checken
+        pass
 
     return True
 
 
-def main():
-    with os.scandir('plugins') as plugintypelists:
-        for plugintype in plugintypelists:
-            if plugintype.is_dir():
-                with os.scandir('plugins/' + plugintype.name) as plugins:
+def iterate_plugins(host, extended_check=False):
+    with os.scandir('plugins') as plugin_type_lists:
+        for plugin_type in plugin_type_lists:
+            if plugin_type.is_dir():
+                # check index of option if available (some have index.php files by default so don't need to check these)
+                with os.scandir('plugins/' + plugin_type.name) as plugins:
                     for plugin in plugins:
                         if plugin.is_dir():
-                            subname = plugin.name[len(plugintype.name) + 1:]
-                            if test_plugin(subname, plugintype.name):
+                            sub_name = plugin.name[len(plugin_type.name) + 1:]
+                            if test_plugin(host, sub_name, plugin_type.name, extended_check):
                                 print(plugin.name)
+
+
+def nail_moodle_version(host, extended_check=False):
+    # compare file changes to the fingerprints create hashlist for files to avoid duplicated checks
+    # count matches for each version to be able to find a possible match if some files got deleted or changed.
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser("moodle-scanner")
+    parser.add_argument("--host", help="Moodle host")
+    parser.add_argument("--extended", help="Check for versions", type=bool)
+    args = parser.parse_args()
+    iterate_plugins(args.host, args.extended)
 
 
 if __name__ == '__main__':
